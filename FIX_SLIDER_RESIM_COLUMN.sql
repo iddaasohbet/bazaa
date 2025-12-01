f2a06178589add7724a2fc320149cb4587ea9fbc
iddaasohbet <iddaasohbet@users.noreply.github.com>
2025-12-01 22:50:14 +0300

    Fix: Slider resim kolonu LONGTEXT'e Ã§evrildi - base64 resim desteÄŸi

---
+++ FIX_SLIDER_RESIM_COLUMN.sql
@@ -0,0 +1,10 @@
+-- Slider tablosundaki resim kolonunu LONGTEXT'e çevir
+-- Base64 resimleri için VARCHAR(500) yeterli değil
+
+ALTER TABLE `slider` 
+MODIFY COLUMN `resim` LONGTEXT NULL COMMENT 'Slider resmi (base64 veya URL)';
+
+-- Link kolonu için de büyütme
+ALTER TABLE `slider` 
+MODIFY COLUMN `link` TEXT NULL COMMENT 'Slider hedef linki';
+
